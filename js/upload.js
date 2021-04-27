79aa31626c2c8d4c1085a36d531d4e2550ce8276
Phoenix787 <shiny777@bk.ru>
2021-04-27 15:56:59 +0500

    add upload

---
+++ js/upload.js
@@ -0,0 +1,48 @@
+(function () {
+
+	let serverStatus = {
+		SUCCESS: 200,
+		BAD_REQUEST: 400,
+		UNAUTHORIZED: 401,
+		NOT_FOUND: 404,
+		EXTERNAL: 500
+	}
+
+	let onError = (message) => {
+		console.error(message);
+	}
+
+	let URL = 'https://javascript.pages.academy/code-and-magick';
+
+	window.upload = (data, onSuccess) => {
+		let xhr = new XMLHttpRequest();
+		xhr.responseType = 'json';
+
+		xhr.addEventListener('load', (evt) => {
+			switch (xhr.status) {
+				case serverStatus.SUCCESS:
+					onSuccess(xhr.response);
+					break;
+				case serverStatus.BAD_REQUEST:
+					onError('Неправильный запрос');
+					break;
+				case serverStatus.UNAUTHORIZED:
+					onError('Пользователь не авторизован');
+					break;
+				case serverStatus.NOT_FOUND:
+					onError('Ничего не найдено');
+					break;			
+			
+				default:
+					onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+					break;
+			}			
+		});
+
+		xhr.open('POST', URL);
+		xhr.send(data);	
+	};
+	xhr.addEventListener('error', (evt) => {
+		onError('Произошла ошибка соединения');
+	});
+})();
